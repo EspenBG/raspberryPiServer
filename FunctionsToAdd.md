e2a929547598527f58d2072865ce6bfbeeede0a3
EspenBG <espen.godo@gmail.com>
2020-11-12 12:53:55 +0100

    Wip/test project (#1)
    * Added example code for the project
    
    * Added branches to the README.md
    
    * Started on the script for the server.
    -Added a new file FunctionsToAdd.md containing information of future implementations to the server
    
    * Started on the script for the server.
    -Added a new file FunctionsToAdd.md containing information of future implementations to the server
    
    * Started on the script for the server.
    -Added a new file FunctionsToAdd.md containing information of future implementations to the server
    
    * Started on the script for the server.
    -Added a new file FunctionsToAdd.md containing information of future implementations to the server
    
    * Started on the script for the server.
    -Added a new file FunctionsToAdd.md containing information of future implementations to the server
    
    * Added FunctionsToAdd.md
    
    * Started testing on the server.js and adding functionality.
    - The only thing that works now is connecting to the socket server
    
    * Added admin users, that can ask for data from the server
    - it is now possible to send data to the server
    - There is no logging at this point in time
    
    * Added admin users, that can ask for data from the server
    - it is now possible to send data to the server
    - There is no logging at this point in time
    
    * Fixed problem with receiving requests from webpage
    
    * Added the possibility to send data to the webpage
    - It does not send the correct data
    - Sends place holder data now
    - Started a draft of how the data can be stored in firebase
    
    * Added the possibility to send data to the webpage
    - It does not send the correct data
    - Sends place holder data now
    - Started a draft of how the data can be stored in firebase
    
    * Added to the FunctionsToAdd.md descriptions, to better describe the scope of the project.
    - Added more TODOs in the main script.js, with grading of the importance
    
    * Started on exporting of data to a JSON database
    - Can read the json file
    - Can not write to the file yet
    - Added more text to multiple READMEs
    
    * Continued working on writing of sensor data to database
    
    * Can now store sensor data in a local JSON database.
    - added a server-config.json, not used yet.
    
    * Cleaned up the code. Removed various comments that are not applicable anymore.
    
    * Added more documentation.
    - changed Id to ID in all instances
    - Added more TODOs to the JSClient.js and script.js
    
    * Fixed a bug in script.js, when receiving data.
    
    * Finished
    
    * Last commit for test-project
    - changed some text in the FunctionsToAdd.md
    
    Co-authored-by: espen <EspenBG>

---
+++ FunctionsToAdd.md
@@ -0,0 +1,270 @@
+# Functions to add to the server or webpage
+## Functions made during the test project and scope of the test project
+### Scope of the test project
+We decided to have a test project to have a framework to build upon, and get an overview of how the project could be done.
+The test project was to have a sensor connected to a unit. The unit communicated with the server, and use a simple webpage to show the value of the sensor.
+There was multiple function that needed to be implemented to accomplish this goal. The functions that was needed was the following:
+
+[//]: # (Add links to the functions to the apliccable descriptions)
+
+- The unit needed to read the sensors value on an analog input on the controller unit.
+- The measured value must be sent to the server, and stored in some way on the server.
+- The webpage need to read the stored values, either send a request to the server or directly access to the data.
+- The webpage needs to show the values in a simple form. This can for example be a table.
+
+### Measure the sensor value
+[//]: # (TODO: Wirte a breef description of how to mesure the sensor value)
+
+
+### Communication between the unit and the main server
+[//]: # (TODO: Wirte a breef description of the communication protocol)
+The communication between the robot server and the robots uses socket.io. The following socket-io events need to work:
+
+#### Sending of sensor data to server
+The event to send data to the server is 'temperature', were the sensor data that is sent is packaged as a JSON string.
+The following example is to send a temperature with a JS robot:
+
+    socket.emit('temperature', '{
+        "sensorID": "#####1", 
+        "temperature": 23.5,
+        }'
+    );
+     
+Change #####1 and 23.5 to the values for the sensor. Were the sensorID is a unique identifier. 
+The sensor identifiers use is for the server to identify and store the value of the correct sensor.
+
+
+### Storing of the sensor data
+[//]: # (TODO: Wirte a breef description of the database usage)
+
+The sensor data should be stored in a local .json file, or on an external service (like Firebase). 
+The structure of the database is a hierarchy were the top-level is sensorID. 
+Every unique sensorID has an entry under sensorID, that stores all the sensor data. 
+Every data entry contains the value of the sensor and a timestamp. See example below for the database setup. 
+In the test project there will only be one sensor. 
+
+    {
+      "SensorID": {
+        "#####1": [
+          {
+            "value": 25.2,
+            "time": 3214214
+          },
+          {
+            "value": 25.2,
+            "time": 3214214
+          }
+        ]
+      }
+    }
+
+
+### Access of the sensor data
+[//]: # (TODO: Wirte a breef description of how the webpage can access the stored data)
+
+
+### Webpage design
+[//]: # (TODO: Wirte a breef description of the webpage design)
+
+
+## Communication between a unit and main server
+This is a brief description of the communication protocol used between units and the robot server.
+
+The communication between a unit (ESP-32) and the main server are using socket.io. 
+The first message from the unit after the connection has been established needs to be the passcode in the following format:
+       
+    'authentication': passcode (str)
+
+The unit will receive the following message as replay if the passcode is correct:
+
+    'authentication': true (bool)
+
+And if the passcode is wrong:
+
+    'authentication': false (bool)
+    
+After the unit has been authenticated it needs to send the unitID in the following format:
+
+    "unitID": "####1"
+    
+The unitID do NOT need to be unique, but there may occur some problems if it is not unique. When the webserver has received and verified. 
+The robot server then sends the setpoint to the unit in the following format:
+
+    "Setpoint": setpointMessage
+
+Setpoint is the event, and the setpointMessage is a string in JSON format, with one line per senosor, like this:
+
+    {
+        "uniqeSensorID": setpoint,
+        "uniqeSensorID2": setpoint2,
+    }
+         
+Where uniqeSensorID is replaced by the sensorID of the first sensor for the unit, and setpoint is replaced by the value of the
+setpoint for that sensor. E.g. if the configuration of the unit has only one sensor,  the ID of this sensor is #####1, and 
+the setpoint is 23.5 degrees the JSON message would be as follows with the event tag Setpoint:
+
+     {"#####1": 23.5}
+     
+
+When the unit has received the setpoints it can start transmitting sensor data to the server in the following format:
+
+    'SensorData': sensorData
+    
+Where sensorData is formatted in JSON and need to include the unitID as a number between 000 and 999, 
+sensorID as a number between 000 and 999 and 
+temperature as a number between -50 and 250 (degrees celsius). It is possible to send larger values, but the system is not designed for this.
+See the example below.
+
+    {
+    "sensorID": "####1",
+    "value": 25.3,
+    }
+    
+    
+## Server
+### Configuration of units/sensors
+The server-config.json in the config directory is the setup file for the server, and contains all the configuration for the sensors and what the sensors control.
+There is also the user configuration in the file, this part contains all the userIDs for the server and what sensors each user can monitor and control. 
+
+[//]: # (TODO: Fix JSON format..)
+
+    {
+        "sensor-config": {
+            "uniqeSensorID": {
+                    "unit": "unit1"
+                    "type": "temperature",
+                    "controlType": "direct",
+                    "output": true,
+                    "setpoint": 23.0
+                },
+            "uniqeSensorID2": {
+                    "unit": "unit1"
+                    "type": "co2",
+                    "controlType": "reversed",
+                    "output": true,
+                    "setpoint": 23.0
+                }
+            }
+        },
+        "user-config": {
+            "uniqeUserID": {              
+                "sensors": {  
+                    "uniqeSensorID": "AliasForSensor",
+                    "uniqeSensorID2": "AliasForSensor2" 
+                }                          
+            },
+            "uniqeUser2ID": {                           
+                "sensors": {                           
+                    "uniqeSensorID": "Alias2"  
+                }                                                                        
+            }                                          
+        }
+    }
+
+    
+
+### Saving data to Firebase
+The server sends all sensor data received to the Firebase realtime database. The server adds a timestamp to the sensor data.
+The structure of the database is like the example below:
+
+    {
+        "timestamps": {
+            "1603573636239": {
+                "unitID": "1",
+                "senorID": "1",
+                "temperature": "24.3",
+            },
+            "16035736374208": {
+                 "unitID": "1",
+                 "senorID": "1",
+                 "temperature": "25.3",   
+            }
+    } 
+    
+    {   
+        "SenorID": {
+            "uniqeSensorID": {
+                    "timestamp": 132321412,
+                    "temperature": "24.3"
+                },
+                {
+                    "timestamp": 43546363,
+                    "temperature": "25.3"
+                },
+                {
+                    "timestamp": 344567867823,
+                    "temperature": "25.3"
+                }
+            },
+            
+            "uniqeSensorID1": {
+                    "timestamp": 5684521862,
+                    "temperature": "23.2"
+                },
+                {
+                    "timestamp": 785768767865,
+                    "temperature": "20"
+                },
+                {
+                    "timestamp": 344565678867823,
+                    "temperature": "95"
+                }
+            }
+        }
+    }
+                
+                                
+
+### Authentication and security
+Need an ability to authenticate a user, or unit (ESP-32). This can be done by a user specific randomly generated passcode. 
+For example using a 16 character long string generated by the server, and then whitelist the unit using this passcode. 
+This means the first communication from a unit (ESP-32) to the server has to be the 'passcode'. Or else the server will block (blacklist) the communication from that ip.
+If the passcode is not used for a certain timeperiod it needs to be deleted. E.g. if the passcode has not been used for 1 week
+the passcode is deleted, and the user needs to generate a new one from the webpage if the unit is going to be used again later.
+
+This is only for communication to the robot server, not the webserver that is running on a different port.
+
+An alternative is to have one passcode per user. This can produce a security hole, unless all the "links" are secure (ssl).
+## Webpage
+### Generate a sensor ID
+There can be added a function for the user to add a new sensor configuration. There should not be the option to edit a sensor that has already implemented.
+The options for adding a new sensor are:
+
+- sensorID: This can also be generated automatically
+- unitID: Can be in a dropdown list, or typed manually 
+- type: Dropdown with the option for temperature or CO2
+- monitor: when enabled the sensor is only for monitoring
+
+If monitor is false, the following options are available:
+
+- control type: reverse (i.e heating a room) or direct (i.e cooling a room) 
+- output: true/false           
+- setpoint: The setpoint for the controller
+
+### Generate a passcode for a unit
+There need to be an option to generate a new passcode for a new unit, when a user has logged in.
+This can for example be a button (new unit), that when pressed asks the main server for a new passcode and displays it in a window.
+ 
+Se entry [Authentication and security](#Authentication and security) for specific information.
+ 
+ ### Access sensor data
+ There needs to be a place to show what sensors the user has access to. The table/graph needs to be generated from a user selection 
+ of sensors and timeperiod.
+ 
+ The communication between the client and the robot server to get sensor data is in the following format:
+ 
+    "getData", sensorSettings
+    
+Where getData is the event and sensorSettings is a JSON object formatted as a string. The sensorSettings can contain the 
+the following settings:
+
+- startTime: the start time in ms
+- stopTime: the stop time in ms
+- sensorID: the ID of the sensor to get information for
+ 
+ 
+            {
+            'timeInterval': "0",
+            'unitIDs': "1",
+            'sensorIDs': "#####1"n
+            }
--- script.js
+++ script.js
@@ -1,58 +1,280 @@
+/***********************************************************************************************************************
+ * ROBOT SERVER
+ * THIS IS A PROGRAM FOR CONTROLLING ROBOTS AND COMMUNICATING WITH A WEBSERVER
+ * WRITTEN AS A PART OF THE SUBJECT IELEA2001
+ ***********************************************************************************************************************/
+
+/*********************************************************************
+ * IMPORTS AND CONSTANTS
+ *********************************************************************/
+
+// TODO Add some of the options to the server-config
+/*
+ * Options for the server-config
+ * Port number
+ * database paths
+ * Passcodes / May be moved to a DB, then the path goes in the server-config
+ * Passcode for WebServer
+ *
+ */
+
+/*
+ * Options for the unit-config, this is used as a DB
+ * unitID: sensor1, sensor2, sensor3
+ */
+
+const app = require('express')();
+const server = require('http').createServer(app);
+const io = require('socket.io').listen(server);
+const _ = require('underscore');
+const fs = require('fs');
+//const jQuery = require('jQuery') // NOT USED ANYMORE
+const session = require('express-session');
+const passport = require('passport');
+
+const sensorDatabase = 'database/sensor-data.json'; // This is the path to the sensor database //TODO move to server-config
+let newSensorData = {SensorID: {}};  // Make the SensorID object where each sensor has there own object, see README for structure.
+
+const roomForAuthentication = 'unsafeClients';
+let unusedPasscodes = [123456789, 123456788];
+let webserverNamespace = io.of('/webserver');
+const serverPort = 3000;
+
+const adminNamespace = io.of('/admin');
+// TODO add the ability to logg other datasets
+
+/*********************************************************************
+ * MAIN PROGRAM
+ *********************************************************************/
+
+webserverNamespace.use((socket, next) => {
+    // ensure the user has sufficient rights
+    // TODO add logic to check if the admin has the correct rights
+    let test = socket;
+    if (socket.request.user) {
+        next();
+    } else {
+        next(new Error('unauthorized'))
+    }
+    console.log("Admin Logged in")
+    //next();
+});
 
 
-var wsUri = "wss://echo.websocket.org/";
-var output;
+// If there is an connection from an admin this runs
+webserverNamespace.on('connection', socket => {
+    socket.on('getData', settings => {
+        addDataToDB(sensorDatabase, newSensorData);
+        console.log('Data request received from admin')
+        let timeInterval = [0];   // is an array containing the start time and stop time
+        let unitIDs = [0];        // is an array containing all the unitIDs to get sensor data for
+        let sensorIDs = [0];      // is an array containing all the sensorIDs to get data for
+        let parsedSettings = JSON.parse(settings)
 
-function init()
-{
-    output = document.getElementById("output");
-    testWebSocket();
+
+        if (parsedSettings.hasOwnProperty('timeInterval')) {
+            timeInterval = parsedSettings.timeInterval;
+        }
+        if (parsedSettings.hasOwnProperty('unitIDs')) {
+            unitIDs = parsedSettings.unitIDs;
+        }
+        if (parsedSettings.hasOwnProperty('sensorIDs')) {
+            sensorIDs = parsedSettings.sensorIDs;
+        }
+
+        let sensorData = getData(timeInterval, unitIDs, sensorIDs);
+        socket.emit('dataResponse', sensorData);
+    });
+});
+
+
+// This is what runs on all the connections that is NOT in the admin namespace
+io.on('connection', socket => {
+    // TODO: Make the logic for authentication of the clients i.e use passcodes
+
+    // When a client connects to the server it gets sent to the room for unsafe clients
+    let clientID = socket.id;
+    let client = io.sockets.connected[clientID];
+    console.log("Client connected with ID: " + clientID)
+    //client.join(roomForAuthentication);
+    //socket.emit('connected', true);
+    //client.emit('test', 'test text');
+    io.on('test', data => {
+        console.log(data);
+
+    });
+    // TODO: Change the structure of the event, to make i more uniform
+    socket.on('temperature', (data) => {
+        // TODO: format print
+        console.log("Received data from: " + clientID);
+        // The data from the unit get parsed from JSON to a JS object
+        let parsedData = JSON.parse(data);
+
+        let sensorID = parsedData.sensorID;
+        // the data to add is temperature and timestamp
+        let dataObject = {
+            value: parsedData.temperature,
+            time: Date.now(),
+        };
+        let sensorData = {};
+        sensorData[sensorID] = dataObject;
+
+        // TODO: format the measurement in a cleaner way
+        console.log(sensorData);
+        // Creates the sensor name object in the new sensor array if it doesn't exist, and adds the new measurement
+        newSensorData.SensorID[sensorID] = newSensorData.SensorID[sensorID] || [];
+        newSensorData.SensorID[sensorID].push(dataObject);
+
+        //TODO 2: Make function for sending of the data to the database
+        //console.log(parsedData.temperature);
+    });
+});
+
+// Write new sensor data to the database every 60 seconds
+let var1 = setInterval(addSensorsToDB, 60000);
+// Start the server on port specified in the server-config
+// TODO: Add port to server-config
+server.listen(serverPort);
+
+
+/*********************************************************************
+ * PROGRAM FUNCTIONS
+ *********************************************************************/
+
+/**
+ * Function to add sensor data to the database
+ */
+function addSensorsToDB() {
+    addDataToDB(sensorDatabase, newSensorData, (numberOfRecords) => {
+        // Get how many measurements that was added to the database
+
+        Object.keys(numberOfRecords).map((sensor, index) => {
+            // Cycle thru every sensor with measurements that was added
+            let numberToDelete = numberOfRecords[sensor];
+            // Delete the same number of records that was added to the database (deletes from first)
+            newSensorData.SensorID[sensor].splice(0, numberToDelete)
+        });
+    });
 }
 
-function testWebSocket()
-{
-    websocket = new WebSocket(wsUri);
-    websocket.onopen = function(evt) { onOpen(evt) };
-    websocket.onclose = function(evt) { onClose(evt) };
-    websocket.onmessage = function(evt) { onMessage(evt) };
-    websocket.onerror = function(evt) { onError(evt) };
+
+/**
+ * Prints all the connected sockets in the room
+ * @param roomName
+ */
+function printRoomClients(roomName) {
+    let clients = io.in(roomName).connected;
+    for (const socket in clients) {
+        console.log(socket);
+    }
 }
 
-function onOpen(evt)
-{
-    writeToScreen("CONNECTED");
-    doSend("WebSocket rocks");
+
+/**
+ * Function to get data from the database, and returns the data as a JSON file
+ * The data that is returned is controlled by the parameters. If there are any missing parameters
+ * or is invalid (i.e. the stop time is before start time) the default values are used.
+ * @param timeInterval  array containing the start time and the stop time
+ * @param unitIDs       array containing the unitIDs
+ * @param sensorIDs     array containing the sensorIDs
+ * @returns {string}    the encoded JSON string
+ */
+function getData(timeInterval, unitIDs, sensorIDs) {
+    //TODO: set default parameters
+    //TODO: add logic to get data form database
+    //TODO 3: Get stored data from JSON file and return the correct data
+    // Error if there are no sensor data
+    let lastSensorReading = Object.keys(newSensorData.SensorID['#####2']).length - 1;
+    let lastSensorValue = newSensorData.SensorID['#####2'];
+    let test = {
+        SensorID:
+            {
+                '#####2': [
+                    lastSensorValue[lastSensorReading]
+                ]
+            }
+
+    };
+
+    let encodedData = JSON.stringify(test);
+    return encodedData
 }
 
-function onClose(evt)
-{
-    writeToScreen("DISCONNECTED");
+
+/**
+ * Function that retrieves a JSON database from a file path.
+ * This is an asynchronous function, and executes the callback after loading and parsing the database.
+ * @param pathToDb
+ * @param callback
+ * @param error Runs if there is an error on reading the database
+ */
+function getDatabase(pathToDb, callback, error) {
+    // Read the database and parse it from JSON format to JS object.
+    fs.readFile(pathToDb, (err, dataBuffer) => {
+        if (err) throw err;
+        try {
+            const database = JSON.parse(dataBuffer);
+
+            console.log(database);
+            if (callback) callback(database);
+        } catch (SyntaxError) {
+            //Run error code if there is a SyntaxError in the DB. E.g. DB is not in JSON format
+            console.error('Error loading database. No changes has been made to the file.');
+            if (error) error();
+        } finally {
+
+        }
+    });
 }
 
-function onMessage(evt)
-{
-    writeToScreen('<span style="color: blue;">RESPONSE: ' + evt.data+'</span>');
-    websocket.close();
+
+/**
+ * Function that first reads the database stored on the supplied path and add the data supplied to the database.
+ * The function assumes there is only one type of data that is going to be added to the database.
+ * You need to delete the data after it is added to the database, the callback function can be used for this.
+ * @param databasePath
+ * @param newData  - Object contains all the sensor data the first object is the same as the parent object in the database.
+ * @param callback  - The callback function supplies the number of records that is deleted
+ */
+function addDataToDB(databasePath, newData, callback) {
+    // Assumes there is only one type of data,
+    // Variable to store the sensor name and how many records to delete after import to the database
+    let deletedRecords = {};
+
+    // First object is always the dataID, e.g. SensorID
+    let dataName = Object.keys(newData)[0];
+
+    // Read the newest version of the database.
+    getDatabase(databasePath, (database) => {
+        // Merge the new data one sensor at the time
+        Object.keys(newData[dataName]).map((sensor, index) => {
+            deletedRecords[sensor] = 0;
+
+            //Create the data type in the database if it is not there
+            database[dataName] = database[dataName] || {};
+            console.log('Adding data form sensor: ' + sensor);
+
+            // Create the sensor name in the database if it is not there
+            database[dataName][sensor] = database[dataName][sensor] || [];
+
+            // Add every measurement to the database
+            newData[dataName][sensor].forEach((measurement) => {
+                database[dataName][sensor].push(measurement);
+
+                // Count how many records that is added
+                deletedRecords[sensor]++;
+            });
+        });
+
+        //Convert the new database to JSON
+        const jsonDatabase = JSON.stringify(database, null, 2);
+        // Write the new database to the path
+        fs.writeFile(databasePath, jsonDatabase, (err) => {
+            if (err) throw err;
+            console.log('Data written to file');
+        });
+
+        // Callback after the database has been updated, if it is in use
+        if (callback) callback(deletedRecords);
+    });
 }
-
-function onError(evt)
-{
-    writeToScreen('<span style="color: red;">ERROR:</span> ' + evt.data);
-}
-
-function doSend(message)
-{
-    writeToScreen("SENT: " + message);
-    websocket.send(message);
-}
-
-function writeToScreen(message)
-{
-    var pre = document.createElement("p");
-    pre.style.wordWrap = "break-word";
-    pre.innerHTML = message;
-    output.appendChild(pre);
-}
-
-window.addEventListener("load", init, false);
-
